699beaa8e234f5de77e6b3c732437df0a9bca513
Djair235 <djairdecastrofeitosa@gmail.com>
2025-01-15 16:25:19 -0300

    consertando o require do HB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = require("../src/routers/router")
-const handlebarsConfig = require("..//src/dependencies/handlebarsConfig")
+const handlebarsConfig = require("../src/dependencies/handlebarsConfig")
 const app = express()
 
 app.use(router)
