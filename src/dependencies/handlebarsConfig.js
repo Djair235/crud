84cebff1d16299841a1205f08a7ba2f2b1630b03
Djair235 <djairdecastrofeitosa@gmail.com>
2025-01-15 17:03:18 -0300

    arrumando pela sexta vez ( espero q seja a ultima) o HB

--- src/dependencies/handlebarsConfig.js
+++ src/dependencies/handlebarsConfig.js
@@ -6,11 +6,11 @@ function handlebarsConfig(app) {
         "handlebars",
         engine({
             defaultLayout: "main", // Nome do layout principal
-            layoutsDir: path.join(__dirname, "../views/layouts"), // Caminho para layouts
+            layoutsDir: path.join(__dirname, "../../views/layouts"), // Caminho para layouts
         })
     );
     app.set("view engine", "handlebars");
-    app.set("views", path.join(__dirname, "../views")); // Caminho para views
+    app.set("views", path.join(__dirname, "../../views")); // Caminho para views
 }
 
 module.exports = handlebarsConfig;
