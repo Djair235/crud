7c97d7f04d630b9a0d0ba9f54490b7dfd8951ff1
Djair235 <djairdecastrofeitosa@gmail.com>
2025-01-15 16:33:47 -0300

    arrumando (denovo) o HB

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const app = express()
 
 app.use(router)
 handlebarsConfig(app)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.listen(3000, () => {
     console.log("Servidor ONLINE na porta 3000")
--- src/dependencies/handlebarsConfig.js
+++ src/dependencies/handlebarsConfig.js
@@ -1,8 +1,17 @@
-const { engine } = require("express-handlebars")
+const path = require("path");
+const { engine } = require("express-handlebars");
 
 function handlebarsConfig(app) {
-    app.engine("handlebars", engine({ defaultLayout: "main" }))
-    app.set("view engine", "handlebars")
+    app.engine(
+        "handlebars",
+        engine({
+            defaultLayout: "main",
+            layoutsDir: path.join(__dirname, "../views/layouts"), // Caminho para layouts
+            partialsDir: path.join(__dirname, "../views/partials"), // Caso use partials
+        })
+    );
+    app.set("view engine", "handlebars");
+    app.set("views", path.join(__dirname, "../views")); // Caminho para views
 }
 
 module.exports = handlebarsConfig;
