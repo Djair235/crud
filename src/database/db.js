5efeea05eba21af08aa8154b4f3f180e2e0a4665
Djair235 <djairdecastrofeitosa@gmail.com>
2025-01-16 02:17:53 -0300

    arrumando o db connection

--- src/database/db.js
+++ src/database/db.js
@@ -1,21 +1,10 @@
-// const mysql2 = require("mysql2/promise")
-
-// const connection = mysql2.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DBNAME
-// })
-
-// module.exports = connection
-
 const mysql2 = require("mysql2/promise")
 
 const connection = mysql2.createPool({
-    host: "bp2b5trmma0g1ptwublv-mysql.services.clever-cloud.com",
-    user: "urluubulwd5kjjkp",
-    password: "VQkTwRpL7V5wK1lwywOP",
-    database: "bp2b5trmma0g1ptwublv"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME
 })
 
 module.exports = connection
